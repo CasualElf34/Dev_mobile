ffb1e1e00137822c71f695b95305ec26b26a8dcc
CasualElf34 <mael.guillaume.34@gmail.com>
2026-04-16 14:20:39 +0200

    app

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
